6a1518d11de9c50adad9150991d296b196ce6d3d
antonio carlos wronski <54239754+acwronski@users.noreply.github.com>
2020-12-01 15:28:25 -0300

    coloquei style no componente menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,7 +1,26 @@
+import Link from 'next/link';
+
+const linkstyle = {
+    marginRight: 15
+}
+
 const Menu = () => {
     return (
         <div>
-            Menu
+            <link href="/">
+                <a style={linkstyle}>Home</a>
+
+            </link>
+
+            <link href="/sobre">
+                <a style={linkstyle}>Sobre as Orquideas</a>
+
+            </link>
+
+            <link href="/contato">
+                <a style={linkstyle}>Contato</a>
+
+            </link>
         </div>
     )
 }
