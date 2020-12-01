9b3649cadc52d02d785aba3d52fada552fc1105b
antonio carlos wronski <54239754+acwronski@users.noreply.github.com>
2020-12-01 17:15:23 -0300

    coloquei spiner size

--- pages/contato.js
+++ pages/contato.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Spinner} from 'reactstrap';
+import { Spinner } from 'reactstrap';
 
 import Menu from '../components/Menu';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,15 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Contato() {
     return (
         <div>
-            <Menu/>
+            <Menu />
             Contato
-            <Spinner color="dark" />
-            <Spinner color="warning" />
-    
-    
-    
+            <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+            <div class="spinner-grow" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+
+
+
         </div>
-        );
+    );
 
 }
 export default Contato
