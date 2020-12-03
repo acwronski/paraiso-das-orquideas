153233aae7df421301c942bc2abe9d6ca9fa93cc
antonio carlos wronski <54239754+acwronski@users.noreply.github.com>
2020-12-03 13:25:45 -0300

    adicionei ico

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Head from 'next/head';
 import Menu from '../components/Menu';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function HomePage() {
     return (
     <div>
+        <Head>
+            <title>Home Zana Orquideas</title>
+            <meta name='description' content='Site Sobre Orquideas e Metodos de Cultivo'/>
+            <meta name='author' content='Antonio Carlos Wronski'/>
+            
+                
+            
+        </Head>
         <Menu/>
         Home
 
