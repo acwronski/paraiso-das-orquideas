19bba2927761cb7e987a34ec92f7e4616df66c9f
antonio carlos wronski <54239754+acwronski@users.noreply.github.com>
2020-12-04 08:38:56 -0300

    adicionei trez campos de cervicos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Menu from '../components/Menu';
-import {Jumbotron, Container} from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
@@ -21,25 +21,36 @@ function Home() {
                     margin-bottom: 0rem !important;
                 }`}</style>
                 <Container className='text-center'>
-                    <h1 className='display-4'>Serviços da Empresa</h1>
-                    <p className='lead'>Temos tudo que suas orquideas precisam para ficarem lindas</p>
+                    <div>
+                        <h1 className='display-4'>Serviços da Empresa</h1>
+                        <p className='lead'>Temos tudo que suas orquideas precisam para ficarem lindas</p>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                            <h2>Heading</h2>
+                            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                        </div>
+                        <div class="col-lg-4">
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                            <h2>Heading</h2>
+                            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                        </div>
+                        <div class="col-lg-4">
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                            <h2>Heading</h2>
+                            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                        </div>
+                    </div>
                 </Container>
             </Jumbotron>
 
-            <Jumbotron fluid className='servicos2'>
-                <style>{`.servicos2{
-                    padding-top: 80px;
-                    padding-bottom: 80px;
-                    background-color: #00ff00;
-                    color: #fff;
-                    
-                }`}</style>
-                <Container className='text-center'>
-                    <h1 className='display-4'>Como acabar com fungos e cochinilhas</h1>
-                    <p className='lead'>Temos tudo que suas orquideas precisam para ficarem lindas</p>
-                </Container>
-            </Jumbotron>
-            
+
+
 
         </div>
     );
