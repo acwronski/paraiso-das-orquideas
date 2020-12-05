cf4b2a287a5a8a29506aefcb494e5e045801e3b8
antonio carlos wronski <54239754+acwronski@users.noreply.github.com>
2020-12-05 16:41:29 -0300

    adicionei sobre Phalaenopsis

--- pages/sobre.js
+++ pages/sobre.js
@@ -11,6 +11,10 @@ function Sobre() {
             Sobre o Paraiso das Orquideas
             <Spinner color="dark" />
             <Spinner color="warning" />
+            <div>
+                Phalaenopsis
+                Gênero de orquídeas asiáticas, as Phalaenopsis também são conhecidas como “orquídeas-borboletas”, pela semelhança das flores com asas de borboleta. Esse tipo de planta prefere sombra, calor e pouca água. Elas podem florir até três vezes por ano e suas flores duram até três meses. As cores dessa flor variam: branca, rosa, azul, roxa, e há também versões híbridas: branca com rosa, azul com branco, amarelo e rosa. As orquídeas brancas são as mais comuns, enquanto a orquídea amarela é rara, sendo mais difícil de encontrar.
+            </div>
 
 
 
